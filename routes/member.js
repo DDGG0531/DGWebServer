073cbb817567edcf2e025cedbc2724f404bd0d00
JimDong <jimdong@damaiapp.com.tw>
2021-12-10 16:47:17 +0800

    :first commit

---
+++ routes/member.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const MemberModifyMethod = require('../controllers/modify_controller');
+
+
+let memberModifyMethod = new MemberModifyMethod();
+
+router.post('/member', memberModifyMethod.postRegister);
+
+router.post('/member/login', memberModifyMethod.postLogin);
+
+router.put('/member', memberModifyMethod.putUpdate);
+
+router.put('/updateimage', memberModifyMethod.putUpdateImage);
+
+
+module.exports = router;
